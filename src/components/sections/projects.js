477e382589be0eddaaaee769256d3247d4781b2b
Lucas Stuker <126526096+LucasStuker@users.noreply.github.com>
2023-07-27 22:15:49 -0300

    1

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -3,11 +3,19 @@ import Card from '../elements/Card'
 import styles from './projects.module.css'
 import Lp from '../../images/Projects/LP.png'
 import ccxp from '../../images/Projects/CCXP.png'
-import farm from '../../images/Projects/farmacia.png'
+import ToDOList from '../../images/Projects/ToDOList.png'
 function Projects () {
     return (
         <div className = {styles.Projects} id="Projects">
             <h1> Projetos</h1>
+            <Card
+                img={ToDOList} 
+                title='Uma lista de tarefas feita por React '
+                tech='HTML, CSS, JAVASCRIPT E VITA & REACT  ' 
+                description='Uma lista de tarefas com dezenas de funcionalidades' 
+                repo='https://github.com/LucasStuker/ToDoList'
+                site=' https://cool-brioche-80fae3.netlify.app/'
+            />
             <Card
                 img={Lp} 
                 title='Landing Page'
@@ -24,14 +32,7 @@ function Projects () {
                 repo='https://github.com/LucasStuker/DesafioCCXP'
                 site='https://clinquant-daifuku-9e3994.netlify.app/'
             />
-            <Card
-                img={farm} 
-                title='Landing Page de uma farmacia '
-                tech='HTML, CSS E JAVASCRIPT' 
-                description='Desenvolvimento de uma farmacia sendo possivel adcionar e remover medicamentos' 
-                repo='https://github.com/LucasStuker/farm-cia/blob/main/README.md'
-                site=' '
-            />
+          
 
             
         </div>
